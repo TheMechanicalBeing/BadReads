c610ac6900531c6469402e57087f91620b906df3
TheMechanicalBeing <tornike.tsulukidze@gmail.com>
2024-03-13 11:39:44 +0400

    Fix filter when some fields are not specified

--- src/views/storage/routes.py
+++ src/views/storage/routes.py
@@ -17,18 +17,18 @@ def storage_get():
     books = Book.query
 
     if request.method == "POST":
-        if form.validate_on_submit():
-            if title_data := form.title.data:
-                books = books.filter(Book.title.ilike(f"%{title_data}%"))
+        if title_data := form.title.data:
+            books = books.filter(Book.title.ilike(f"%{title_data}%"))
 
-            if author_data := form.author.data:
-                books = books.join(AuthorBook).join(Author).filter(Author.first_name_.ilike(f"%{author_data}%") | Author.last_name_.ilike(f"%{author_data}"))
+        if author_data := form.author.data:
+            books = books.join(AuthorBook).join(Author).filter(
+                Author.first_name_.ilike(f"%{author_data}%") | Author.last_name_.ilike(f"%{author_data}"))
 
-            if publish_from_data := form.publish_from.data:
-                books = books.filter(Book.publication_year >= publish_from_data)
+        if publish_from_data := form.publish_from.data:
+            books = books.filter(Book.publication_year >= publish_from_data)
 
-            if publish_to_data := form.publish_to.data:
-                books = books.filter(Book.publication_year <= publish_to_data)
+        if publish_to_data := form.publish_to.data:
+            books = books.filter(Book.publication_year <= publish_to_data)
 
     books = books.all()
     return render_template("storage.html", books=books, form=form)
